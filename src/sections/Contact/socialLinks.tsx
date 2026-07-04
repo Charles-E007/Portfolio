dfb92f1c706bc570fc9a6d12bd84ae6ec2a5c072
Charles-E007 <charles.loukou@epitech.eu>
2026-07-04 18:44:31 +0000

    add new icon and link of sociallink

--- src/sections/Contact/socialLinks.tsx
+++ src/sections/Contact/socialLinks.tsx
@@ -4,7 +4,7 @@ import type { SlidingLogoMarqueeItem } from "../../components/ui/SlidingLogoMarq
 
 export const socialLinks: SlidingLogoMarqueeItem[] = [
   { id: "github",   href: "https://github.com/Charles-E007",   content: <SiGithub size={36} color="var(--color-primary)" /> },
-  { id: "linkedin", href: "https://linkedin.com/in/TON-PROFIL", content: <FaLinkedin size={36} color="var(--color-primary)" /> },
+  { id: "linkedin", href: "https://www.linkedin.com/in/loukou-charles-6847843a8/", content: <FaLinkedin size={36} color="var(--color-primary)" /> },
   { id: "facebook", href: "https://facebook.com/TON-PROFIL",    content: <SiFacebook size={36} color="var(--color-primary)" /> },
   { id: "tiktok",   href: "https://tiktok.com/@TON-PROFIL",     content: <SiTiktok size={36} color="var(--color-primary)" /> },
   { id: "twitter",  href: "https://twitter.com/TON-PROFIL",     content: <FaTwitter size={36} color="var(--color-primary)" /> },
