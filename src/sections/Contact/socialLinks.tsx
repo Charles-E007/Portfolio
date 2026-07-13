8342875f25a1e6303efc85a3406696ba6e7a2260
Charles-E007 <charles.loukou@epitech.eu>
2026-07-13 14:04:25 +0000

    change disposition of grid skill

--- src/sections/Contact/socialLinks.tsx
+++ src/sections/Contact/socialLinks.tsx
@@ -3,9 +3,9 @@ import { FaLinkedin, FaTwitter } from "react-icons/fa6" // Changement ici pour Ã
 import type { SlidingLogoMarqueeItem } from "../../components/ui/SlidingLogoMarquee"
 
 export const socialLinks: SlidingLogoMarqueeItem[] = [
-  { id: "github",   href: "https://github.com/Charles-E007",   content: <SiGithub size={36} color="var(--color-primary)" /> },
-  { id: "linkedin", href: "https://www.linkedin.com/in/loukou-charles-6847843a8/", content: <FaLinkedin size={36} color="var(--color-primary)" /> },
-  { id: "facebook", href: "https://facebook.com/TON-PROFIL",    content: <SiFacebook size={36} color="var(--color-primary)" /> },
-  { id: "tiktok",   href: "https://tiktok.com/@TON-PROFIL",     content: <SiTiktok size={36} color="var(--color-primary)" /> },
-  { id: "twitter",  href: "https://twitter.com/TON-PROFIL",     content: <FaTwitter size={36} color="var(--color-primary)" /> },
+  { id: "github",   href: "https://github.com/Charles-E007",   content: <SiGithub size={36} color="#F5F0E8" /> },
+  { id: "linkedin", href: "https://www.linkedin.com/in/loukou-charles-6847843a8/", content: <FaLinkedin size={36} color="#0A66C2" /> },
+  { id: "facebook", href: "https://facebook.com/TON-PROFIL",    content: <SiFacebook size={36} color="#1877F2" /> },
+  { id: "tiktok",   href: "https://tiktok.com/@TON-PROFIL",     content: <SiTiktok size={36} color="#FE2C55" /> },
+  { id: "twitter",  href: "https://twitter.com/TON-PROFIL",     content: <FaTwitter size={36} color="#1DA1F2" /> },
 ]
